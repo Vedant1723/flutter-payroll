c3e9edefd4fed9af09e1b9bac3f4697d3e8344e3
vedant1723 <vedant.pruthi1723@gmail.com>
2021-07-07 13:26:28 +0530

    Screens updated

--- lib/components/Auth/SignupPage.dart
+++ lib/components/Auth/SignupPage.dart
@@ -30,195 +30,197 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Expanded(
-          child: Form(
-            key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.all(25.0)),
-                Icon(
-                  Icons.task_rounded,
-                  size: 60.0,
-                ),
-                Padding(padding: EdgeInsets.all(10.0)),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.name,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Business Name",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.business)),
-                    onSaved: (input) => businessName = input!,
+      body: Container(
+        child: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.all(25.0)),
+                  Icon(
+                    Icons.task_rounded,
+                    size: 60.0,
                   ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.name,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Owner Name",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.person)),
-                    onSaved: (input) => ownerName = input!,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.name,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Business Type",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.merge_type)),
-                    onSaved: (input) => email = input!,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Number of Employees",
-                      icon: Icon(Icons.people),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.name,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Business Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.business)),
+                      onSaved: (input) => businessName = input!,
                     ),
-                    onSaved: (input) => noOfEmployees = input!,
                   ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.mail)),
-                    onSaved: (input) => email = input!,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Phone",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.phone)),
-                    onSaved: (input) => phone = input! as int?,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    obscureText: !showPassword,
-                    keyboardType: TextInputType.emailAddress,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: showPassword
-                              ? new Icon(Icons.no_encryption_sharp)
-                              : new Icon(Icons.enhanced_encryption),
-                          onPressed: viewPassword,
-                        )),
-                    onSaved: (input) => password = input!,
-                  ),
-                ),
-                Padding(padding: EdgeInsets.all(10.0)),
-                ButtonTheme(
-                    minWidth: 200.0,
-                    height: 45.0,
-                    child: RaisedButton(
-                      color: Colors.black12,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.blue)),
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("OTP Sent!"),
-                        ));
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (BuildContext context) => OtpPage()));
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.name,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
                       },
-                      splashColor: Colors.black,
-                      child: Text("Signup"),
-                    )),
-                Padding(padding: EdgeInsets.all(10.0)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("Already Have an accout? "),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()));
+                      decoration: InputDecoration(
+                          labelText: "Owner Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.person)),
+                      onSaved: (input) => ownerName = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.name,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
                       },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
+                      decoration: InputDecoration(
+                          labelText: "Business Type",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.merge_type)),
+                      onSaved: (input) => email = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Number of Employees",
+                        icon: Icon(Icons.people),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                    )
-                  ],
-                )
-              ],
+                      onSaved: (input) => noOfEmployees = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.mail)),
+                      onSaved: (input) => email = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Phone",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.phone)),
+                      onSaved: (input) => phone = input! as int?,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      obscureText: !showPassword,
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: showPassword
+                                ? new Icon(Icons.no_encryption_sharp)
+                                : new Icon(Icons.enhanced_encryption),
+                            onPressed: viewPassword,
+                          )),
+                      onSaved: (input) => password = input!,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  ButtonTheme(
+                      minWidth: 200.0,
+                      height: 45.0,
+                      child: RaisedButton(
+                        color: Colors.black12,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.blue)),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("OTP Sent!"),
+                          ));
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => OtpPage()));
+                        },
+                        splashColor: Colors.black,
+                        child: Text("Signup"),
+                      )),
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Already Have an accout? "),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/components/Employees/AddEmployee.dart
+++ lib/components/Employees/AddEmployee.dart
@@ -24,136 +24,144 @@ class _AddEmployeeState extends State<AddEmployee> {
       appBar: AppBar(
         title: Text("Add Employee"),
       ),
-      body: Center(
-        child: Expanded(
-          child: Form(
-            key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.all(10.0)),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.name,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Name",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.person)),
-                    onSaved: (input) => name = input!,
+      body: Container(
+        child: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.all(25.0)),
+                  Icon(
+                    Icons.people,
+                    size: 60.0,
                   ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.email)),
-                    onSaved: (input) => email = input!,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.number,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Phone",
-                      icon: Icon(Icons.phone),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onSaved: (input) => phone = input! as int,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Address",
-                      icon: Icon(Icons.location_city),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onSaved: (input) => address = input!,
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    keyboardType: TextInputType.number,
-                    enableSuggestions: true,
-                    validator: (input) {
-                      if (input == "") {
-                        return 'Field is Empty';
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Salary",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        icon: Icon(Icons.payment)),
-                    onSaved: (input) => email = input!,
-                  ),
-                ),
-                DropdownButton<String>(
-                    items: locations.map((String val) {
-                      return new DropdownMenuItem<String>(
-                        value: val,
-                        child: new Text(val),
-                      );
-                    }).toList(),
-                    hint: Text("Please choose a Salary Type"),
-                    onChanged: (newVal) {
-                      // _selectedLocation = newVal;
-                      this.setState(() {});
-                    }),
-                Padding(padding: EdgeInsets.all(10.0)),
-                ButtonTheme(
-                    minWidth: 200.0,
-                    height: 45.0,
-                    child: RaisedButton(
-                      color: Colors.black12,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(color: Colors.blue)),
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Employee Added!"),
-                        ));
-                        Navigator.pop(context);
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.name,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
                       },
-                      splashColor: Colors.black,
-                      child: Text("Add Emplloyee"),
-                    )),
-                Padding(padding: EdgeInsets.all(10.0)),
-              ],
+                      decoration: InputDecoration(
+                          labelText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.person)),
+                      onSaved: (input) => name = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.email)),
+                      onSaved: (input) => email = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.number,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Phone",
+                        icon: Icon(Icons.phone),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onSaved: (input) => phone = input! as int,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Address",
+                        icon: Icon(Icons.location_city),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onSaved: (input) => address = input!,
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      keyboardType: TextInputType.number,
+                      enableSuggestions: true,
+                      validator: (input) {
+                        if (input == "") {
+                          return 'Field is Empty';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: "Salary",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          icon: Icon(Icons.payment)),
+                      onSaved: (input) => email = input!,
+                    ),
+                  ),
+                  DropdownButton<String>(
+                      items: locations.map((String val) {
+                        return new DropdownMenuItem<String>(
+                          value: val,
+                          child: new Text(val),
+                        );
+                      }).toList(),
+                      hint: Text("Please choose a Salary Type"),
+                      onChanged: (newVal) {
+                        // _selectedLocation = newVal;
+                        this.setState(() {});
+                      }),
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  ButtonTheme(
+                      minWidth: 200.0,
+                      height: 45.0,
+                      child: RaisedButton(
+                        color: Colors.black12,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.blue)),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Employee Added!"),
+                          ));
+                          Navigator.pop(context);
+                        },
+                        splashColor: Colors.black,
+                        child: Text("Add Emplloyee"),
+                      )),
+                  Padding(padding: EdgeInsets.all(10.0)),
+                ],
+              ),
             ),
           ),
         ),
